369cdc057a41b61a3592f60511c8338146972331
Neo <Neo@DESKTOP-PJ7I3TS>
2023-02-09 17:44:06 +0800

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For loop with extracting numbers divisible by 5.
+            //Check if this will be pushed in new Git hub repository.
+        }
+    }
+}
