3fbb8112c4748cbf97a2648d70f3ac3b48390b22
Neo <Neo@DESKTOP-PJ7I3TS>
2023-02-09 17:47:00 +0800

    added comments for commit testing in github

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp3
         {
             //For loop with extracting numbers divisible by 5.
             //Check if this will be pushed in new Git hub repository.
+
+            //first commit to github repo
         }
     }
 }
